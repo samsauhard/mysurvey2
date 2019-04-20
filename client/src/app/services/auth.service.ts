f6d8d5f5acb83724a43abb67fbbaaff5bedd1cda
samsauhard <samsauhard1@gmail.com>
2019-04-20 11:43:45 -0400

    s

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -13,7 +13,6 @@ export class AuthService {
 
   private endpoint = 'https://portfoliomean.herokuapp.com/api/';
 
-
   private httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
